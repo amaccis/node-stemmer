8cd9f53467063ae44a0706a3e5d8661c27c2a9d1
Andrea Maccis <andrea.maccis@gmail.com>
2025-05-16 00:13:48 +0200

    Refactor sources

--- src/libstemmer.ts
+++ src/libstemmer.ts
@@ -1,46 +1,87 @@
-import * as ffi from 'ffi-napi';
-import * as ref from 'ref-napi';
-import * as struct from 'ref-struct-di';
+import { open, load, DataType, JsExternal, arrayConstructor } from 'ffi-rs';
 
 export class Libstemmer {
  
     static getLibstemmer() 
     {
-        const StructType = struct(ref);
-        const sb_stemmer = StructType();
+        const LIBRARY = 'libstemmer';
 
-        return ffi.Library('libstemmer', {
-            'sb_stemmer_list': [ 
-                ref.coerceType('char **'),
-                [] 
-            ],
-            'sb_stemmer_new': [
-                ref.refType(sb_stemmer),
-                [
-                    ref.types.CString,
-                    ref.types.CString
-                ]
-            ],
-            'sb_stemmer_delete': [
-                ref.types.void, 
-                [ 
-                    ref.refType(sb_stemmer) 
-                ]
-            ],
-            "sb_stemmer_stem": [ 
-                ref.refType(ref.types.uchar), 
-                [ 
-                    ref.refType(sb_stemmer), 
-                    ref.types.CString,
-                    ref.types.int 
-                ] 
-            ],
-            'sb_stemmer_length': [ 
-                ref.types.int, 
-                [ 
-                    ref.refType(sb_stemmer) 
-                ] 
-            ]
+        open({
+            library: LIBRARY,
+            path: 'libstemmer.so'
         });
+
+        return {
+            sb_stemmer_list: () => {
+                return load({
+                    library: LIBRARY,
+                    funcName: 'sb_stemmer_list',
+                    retType: arrayConstructor({
+                        type: DataType.StringArray,
+                        length: 32  // FIXME 
+                    }),
+                    paramsType: [],
+                    paramsValue: []
+                })
+            },
+            sb_stemmer_new: (algorithm: string, charenc: string) => {
+                return load({
+                    library: LIBRARY,
+                    funcName: 'sb_stemmer_new',
+                    retType: DataType.External,
+                    paramsType: [
+                        DataType.String, 
+                        DataType.String
+                    ],
+                    paramsValue: [
+                        algorithm, 
+                        charenc
+                    ]
+                })
+            },
+            sb_stemmer_stem: (stemmer: JsExternal, word: Buffer, size: number) => {
+                return load({
+                    library: LIBRARY,
+                    funcName: 'sb_stemmer_stem',
+                    retType: DataType.String,
+                    paramsType: [
+                        DataType.External,
+                        DataType.U8Array,
+                        DataType.I32 
+                    ],
+                    paramsValue: [
+                        stemmer,
+                        word,
+                        size
+                    ] 
+                })
+            },
+            sb_stemmer_length: (stemmer: JsExternal) => {
+                return load({
+                    library: LIBRARY,
+                    funcName: 'sb_stemmer_length',
+                    retType: DataType.I32,
+                    paramsType: [ 
+                        DataType.External 
+                    ],
+                    paramsValue: [
+                        stemmer
+                    ]
+                })
+            },
+            sb_stemmer_delete: (stemmer: JsExternal) => {
+                return load({
+                    library: LIBRARY,
+                    funcName: 'sb_stemmer_delete',
+                    retType: DataType.Void,
+                    paramsType: [
+                        DataType.External
+                    ],
+                    paramsValue: [
+                        stemmer
+                    ]
+                })
+            }
+        }
     }
 }
--- test/stemmer.test.ts
+++ test/stemmer.test.ts
@@ -1,104 +1,352 @@
 import Stemmer from '../src/stemmer'
+import { UnavailableAlgorithmError } from '../src/error/unavailable-algorithm-error';
+import { CharacterEncoding } from '../src/enum/character-encoding';
+import { encode } from 'iconv-lite';
 
-describe("test algorithms() method", () => {
-    const algorithms = [
-        'arabic',
-        'armenian',
-        'basque',
-        'catalan',
-        'danish',
-        'dutch',
-        'english',
-        'finnish',
-        'french',
-        'german',
-        'greek',
-        'hindi',
-        'hungarian',
-        'indonesian',
-        'irish',
-        'italian',
-        'lithuanian',
-        'nepali',
-        'norwegian',
-        'porter',
-        'portuguese',
-        'romanian',
-        'russian',
-        'serbian',
-        'spanish',
-        'swedish',
-        'tamil',
-        'turkish',
-        'yiddish'
-    ];
-    test(`should return these algorithms: ${algorithms}`, () => {
-        expect(Stemmer.algorithms()).toStrictEqual(algorithms)
+describe('test static method algorithms()', () => {
+    test('should return an array not empty', () => {
+
+        const algorithm = 'english';
+        const word = Buffer.from('cycling');
+        const stemmer = new Stemmer(algorithm); // default character encoding is UTF-8
+        const stem = stemmer.stemWord(word);
+        console.log(stem);
+
+
+        const algorithms = Stemmer.algorithms();
+        expect(algorithms).toBeInstanceOf(Array);
+        expect(algorithms.length).toBeGreaterThan(0);
     });
 });
 
-const stemWordData = [
+describe('test new Stemmer()', () => {
+    test(`should throw Error if the algorithm is unavailable`, () => {
+        expect(() => {new Stemmer('dothraki')}).toThrow(UnavailableAlgorithmError);
+    }),
+    test(`should throw Error if the algorithm is available but the encoding is not`, () => {
+        expect(() => {new Stemmer('italian', CharacterEncoding.KOI8_R)}).toThrow('Unavailable algorithm');
+    })
+});
+
+const stemWordUtf8Charenc = [
     {
-        algorithm: 'english',
-        word: 'cycling',
+        algorithm: 'english', 
+        word: 'cycling', 
         stem: 'cycl'
     },
     {
-        algorithm: 'italian',
-        word: 'camminare',
+        algorithm: 'italian', 
+        word: 'camminare', 
         stem: 'cammin'
     },
     {
-        algorithm: 'portuguese',
-        word: 'atribuição',
+        algorithm: 'portuguese', 
+        word: 'atribuição', 
         stem: 'atribuiçã'
+    },
+    {
+        algorithm: 'basque', 
+        word: 'aberatsenetakoa', 
+        stem: 'aberatse'
+    },
+    {
+        algorithm: 'catalan', 
+        word: 'arruïnada', 
+        stem: 'arru'
+    },
+    {
+        algorithm: 'danish', 
+        word: 'afbildningerne', 
+        stem: 'afbildning'
+    },
+    {
+        algorithm: 'hungarian', 
+        word: 'lenyűgözőnek', 
+        stem: 'lenyűgöző'
+    },
+    {
+        algorithm: 'romanian', 
+        word: 'luminișurile', 
+        stem: 'luminișur'
+    },
+    {
+        algorithm: 'russian', 
+        word: 'взъерошенный', 
+        stem: 'взъерошен'
     }
 ];
-describe.each(stemWordData)(`test stemWord() method`, (item) => {
-    test(`using ${item.algorithm} algorithm for the word ${item.word} the stem should be ${item.stem}`, () => {
+describe.each(stemWordUtf8Charenc)(`test stemWord() method with no charenc`, (item) => {
+    test(`the algorithm ${item.algorithm} for the word ${item.word} should return the stem ${item.stem}`, () => {
         const stemmer = new Stemmer(item.algorithm);
-        expect(stemmer.stemWord(item.word)).toBe(item.stem);
+        const word = Buffer.from(item.word);
+        expect(stemmer.stemWord(word)).toStrictEqual(Buffer.from(item.stem).toString());
+    });
+});
+describe.each(stemWordUtf8Charenc)(`test stemWord() method with charenc UTF_8`, (item) => {
+    test(`the algorithm ${item.algorithm} for the word ${item.word} should return the stem ${item.stem}`, () => {
+        const stemmer = new Stemmer(item.algorithm, CharacterEncoding.UTF_8);
+        const word = Buffer.from(item.word);
+        expect(stemmer.stemWord(word)).toStrictEqual(Buffer.from(item.stem).toString());
     });
 });
 
-const stemWordsData = [
+const stemWordIso88591Charenc = [
     {
-        algorithm: 'english',
+        algorithm: 'basque', 
+        word: 'aberatsenetakoa', 
+        stem: 'aberatse'
+    },
+    {
+        algorithm: 'catalan', 
+        word: 'arruïnada', 
+        stem: 'arru'
+    },
+    {
+        algorithm: 'danish', 
+        word: 'afbildningerne', 
+        stem: 'afbildning'
+    }
+];
+describe.each(stemWordIso88591Charenc)(`test stemWord() method with charenc ISO_8859_1`, (item) => {
+    test(`the algorithm ${item.algorithm} for the word ${item.word} should return the stem ${item.stem}`, () => {        
+        const stemmer = new Stemmer(item.algorithm, CharacterEncoding.ISO_8859_1);
+        const word = encode(item.word, 'ISO_8859_1');
+        expect(stemmer.stemWord(word)).toStrictEqual(encode(item.stem, 'iso-8859-1').toString());
+    });
+});
+
+const stemWordIso88592Charenc = [
+    {
+        algorithm: 'hungarian', 
+        word: 'lenyűgözőnek', 
+        stem: 'lenyűgöző'
+    }
+];
+describe.each(stemWordIso88592Charenc)(`test stemWord() method with charenc ISO_8859_2`, (item) => {
+    test(`the algorithm ${item.algorithm} for the word ${item.word} should return the stem ${item.stem}`, () => {
+        const stemmer = new Stemmer(item.algorithm, CharacterEncoding.ISO_8859_2);
+        const word = encode(item.word, 'iso-8859-2');
+        expect(stemmer.stemWord(word)).toStrictEqual(encode(item.stem, 'iso-8859-2').toString());
+    });
+});
+
+const stemWordKoi8rCharenc = [
+    {
+        algorithm: 'russian', 
+        word: 'взъерошенный', 
+        stem: 'взъерошен'
+    }
+];
+describe.each(stemWordKoi8rCharenc)(`test stemWord() method with charenc KOI8_R`, (item) => {
+    test(`the algorithm ${item.algorithm} for the word ${item.word} should return the stem ${item.stem}`, () => {
+        const stemmer = new Stemmer(item.algorithm, CharacterEncoding.KOI8_R);
+        const word = encode(item.word, 'koi8-r');
+        expect(stemmer.stemWord(word)).toStrictEqual(encode(item.stem, 'koi8-r').toString());
+    });
+});
+
+const stemWordsUtf8Charenc = [
+    { 
+        algorithm: 'english', 
         words: [
-            'cycling',
+            'cycling', 
             'doors'
-        ],
+        ], 
         stems: [
-            'cycl',
+            'cycl', 
             'door'
         ]
     },
     {
-        algorithm: 'italian',
+        algorithm: 'italian', 
         words: [
-            'camminare',
+            'camminare', 
             'porte'
-        ],
+        ], 
         stems: [
-            'cammin',
+            'cammin', 
             'port'
         ]
     },
-    {
-        algorithm: 'portuguese',
+    {   
+        algorithm: 'portuguese', 
         words: [
-            'atribuição',
+            'atribuição', 
             'obrigações'
-        ],
+        ], 
         stems: [
-            'atribuiçã',
+            'atribuiçã', 
             'obrig'
+        ]
+    },
+    {
+        algorithm: 'basque', 
+        words: [
+            'aberatsenetakoa', 
+            'txotxongilo'
+        ], 
+        stems: [
+            'aberatse', 
+            'txotxongilo'
+        ]
+    },
+    {
+        algorithm: 'catalan', 
+        words: [
+            'gratuïtament', 
+            'cuaespinós'
+        ], 
+        stems: [
+            'gratuit', 
+            'cuaespin'
+        ]
+    },
+    {
+        algorithm: 'danish', 
+        words: [
+            'afbildningerne', 
+            'linnedklæderne'
+        ], 
+        stems: [
+            'afbildning', 
+            'linnedklæd'
+        ]
+    },
+    {
+        algorithm: 'hungarian', 
+        words: [
+            'lenyűgözőnek', 
+            'megháromszorozódott'
+        ], 
+        stems: [
+            'lenyűgöző', 
+            'megháromszorozódot'
+        ]
+    },
+    {
+        algorithm: 'romanian', 
+        words: [
+            'luminișurile', 
+            'personalităţilor'
+        ], 
+        stems: [
+            'luminișur', 
+            'personal'
+        ]
+    },
+    {
+        algorithm: 'russian', 
+        words: [
+            'взъерошенный', 
+            'затруднительное'
+        ], 
+        stems: [
+            'взъерошен', 
+            'затруднительн'
+        ]
+    },
+];
+describe.each(stemWordsUtf8Charenc)(`test stemWords() method with no charenc`, (item) => {
+    test(`the algorithm ${item.algorithm} for the words ${item.words} should return the stems ${item.stems}`, () => {
+        const stemmer = new Stemmer(item.algorithm);
+        const words = item.words.map(word => Buffer.from(word));
+        const stems = item.stems.map(stem => Buffer.from(stem).toString());
+        expect(stemmer.stemWords(words)).toStrictEqual(stems);
+    });
+});
+describe.each(stemWordsUtf8Charenc)(`test stemWords() method with charenc UTF_8`, (item) => {
+    test(`the algorithm ${item.algorithm} for the words ${item.words} should return the stems ${item.stems}`, () => {
+        const stemmer = new Stemmer(item.algorithm, CharacterEncoding.UTF_8);
+        const words = item.words.map(word => Buffer.from(word));
+        const stems = item.stems.map(stem => Buffer.from(stem).toString());
+        expect(stemmer.stemWords(words)).toStrictEqual(stems);
+    });
+});
+
+const stemWordsIso88591Charenc = [
+    {
+        algorithm: 'basque', 
+        words: [
+            'aberatsenetakoa', 
+            'txotxongilo'
+        ], 
+        stems: [
+            'aberatse', 
+            'txotxongilo'
+        ]
+    },
+    {
+        algorithm: 'catalan', 
+        words: [
+            'gratuïtament', 
+            'cuaespinós'
+        ], 
+        stems: [
+            'gratuit', 
+            'cuaespin'
+        ]
+    },
+    {
+        algorithm: 'danish', 
+        words: [
+            'afbildningerne', 
+            'linnedklæderne'
+        ], 
+        stems: [
+            'afbildning', 
+            'linnedklæd'
         ]
     }
 ];
-describe.each(stemWordsData)(`test stemWords() method`, (item) => {
-    test(`using ${item.algorithm} algorithm for the words ${item.words} the stems should be ${item.stems}`, () => {
-        const stemmer = new Stemmer(item.algorithm);
-        expect(stemmer.stemWords(item.words)).toStrictEqual(item.stems);
+describe.each(stemWordsIso88591Charenc)(`test stemWords() method with charenc ISO_8859_1`, (item) => {
+    test(`the algorithm ${item.algorithm} for the words ${item.words} should return the stems ${item.stems}`, () => {
+        const stemmer = new Stemmer(item.algorithm, CharacterEncoding.ISO_8859_1);
+        const words = item.words.map(word => encode(word, 'iso-8859-1'));
+        const stems = item.stems.map(stem => encode(stem, 'iso-8859-1').toString());
+        expect(stemmer.stemWords(words)).toStrictEqual(stems);
+    });
+});
+
+const stemWordsIso88592Charenc = [
+    { 
+        algorithm: 'hungarian', 
+        words: [
+            'lenyűgözőnek', 
+            'megháromszorozódott'
+        ], 
+        stems: [
+            'lenyűgöző', 
+            'megháromszorozódot'
+        ]
+    },
+];
+describe.each(stemWordsIso88592Charenc)(`test stemWords() method with charenc ISO_8859_2`, (item) => {
+    test(`the algorithm ${item.algorithm} for the words ${item.words} should return the stems ${item.stems}`, () => {
+        const stemmer = new Stemmer(item.algorithm, CharacterEncoding.ISO_8859_2);
+        const words = item.words.map(word => encode(word, 'iso-8859-2'));
+        const stems = item.stems.map(stem => encode(stem, 'iso-8859-2').toString());
+        expect(stemmer.stemWords(words)).toStrictEqual(stems);
+    });
+});
+
+const stemWordsKoi8rCharenc = [
+    {
+        algorithm: 'russian', 
+        words: [
+            'взъерошенный', 
+            'затруднительное'
+        ], 
+        stems: [
+            'взъерошен', 
+            'затруднительн'
+        ]
+    }
+];
+describe.each(stemWordsKoi8rCharenc)(`test stemWords() method with charenc KOI8_R`, (item) => {
+    test(`the algorithm ${item.algorithm} for the words ${item.words} should return the stems ${item.stems}`, () => {
+        const stemmer = new Stemmer(item.algorithm, CharacterEncoding.KOI8_R);
+        const words = item.words.map(word => encode(word, 'koi8-r'));
+        const stems = item.stems.map(stem => encode(stem, 'koi8-r').toString());
+        expect(stemmer.stemWords(words)).toStrictEqual(stems);
     });
 });
